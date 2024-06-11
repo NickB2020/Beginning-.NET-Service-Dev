afa24b0e0bbb1a290ad4ef5e8438e02ce53d074e
Nick Bischoff <klaus@neo.rr.com>
2024-06-11 11:33:28 -0400

    Get working code

--- src/IssueTrackerSolution/IssueTracker.Api/Program.cs
+++ src/IssueTrackerSolution/IssueTracker.Api/Program.cs
@@ -1,26 +1,31 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container. - behind the scenes stuff that is our code, or how we are going to connect
+// Add services to the container. - behind the scenes stuff that is our code, or how we are going to connedct
 // attached resources (databases, other apis, etc.)
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build(); // after this, phase 2 we have the "background" stuff set up, now let's talk about
+var app = builder.Build(); // after this, phase 2 - we have the "background" stuff set up, now let's talk about
 // how we are going to handle incoming requests and make responses.
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger(); // the thing that generates our OpenAPI specification (it's a JSON document)
-    app.UseSwaggerUI(); // the thing that let's us look at that and interact with it
+    app.UseSwaggerUI(); // the thing that let's us look at that and interact with it 
 }
 
 app.UseAuthorization();
 
-app.MapControllers(); // Using REFLECTION, go make a "route table" (like a phone list)
+app.MapControllers(); // Using REFLECTION, go make a "route table" (like a phone list.)
+
 
 app.Run(); // That the application is running. This is a blocking call.
-Console.WriteLine("Done running the application");
+
+public partial class Program { } // admittedly weird, but this just means I can see the Program class from my tests.
--- src/IssueTrackerSolution/IssueTracker.Api/Status/Api.cs
+++ src/IssueTrackerSolution/IssueTracker.Api/Status/Api.cs
@@ -6,11 +6,20 @@ public class Api : ControllerBase
 {
     // GET /status
     [HttpGet("/status")]
-    public async Task<ActionResult> GetTheStatusAsync()
+    public async Task<ActionResult> GetTheStatusAsync() // Slime
     {
         var response = new GetStatusResponse("Looks Good Here!", DateTimeOffset.UtcNow);
+        //{
+        //   Message = "Looks Good",
+        //   LastChecked = DateTimeOffset.UtcNow
+        //};
         return Ok(response); // 
     }
 }
 
 public record GetStatusResponse(string Message, DateTimeOffset LastChecked);
+//public record GetStatusResponse
+//{
+//    public required string Message { get; init; } = string.Empty;
+//    public required DateTimeOffset LastChecked { get; init; }
+//}
