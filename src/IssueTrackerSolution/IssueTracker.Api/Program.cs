53c81678886d7ed6ad0462db7f60717c83eef7da
Nick Bischoff <klaus@neo.rr.com>
2024-06-11 11:31:15 -0400

    Fix

--- src/IssueTrackerSolution/IssueTracker.Api/Program.cs
+++ src/IssueTrackerSolution/IssueTracker.Api/Program.cs
@@ -1,23 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. - behind the scenes stuff that is our code, or how we are going to connect
+// attached resources (databases, other apis, etc.)
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+var app = builder.Build(); // after this, phase 2 we have the "background" stuff set up, now let's talk about
+// how we are going to handle incoming requests and make responses.
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(); // the thing that generates our OpenAPI specification (it's a JSON document)
+    app.UseSwaggerUI(); // the thing that let's us look at that and interact with it
 }
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers(); // Using REFLECTION, go make a "route table" (like a phone list)
 
-app.Run();
+app.Run(); // That the application is running. This is a blocking call.
+Console.WriteLine("Done running the application");
